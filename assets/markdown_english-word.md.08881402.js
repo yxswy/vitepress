90623816f497859bd324c048fb5d1b65f61d973b
yxswy <44958959+yxswy@users.noreply.github.com>
2022-06-05 15:21:14 +0000

    Deploying to gh-pages from  @ d1d5ddcf7b195bb3e27d4cb2c4980e2dfb1ae08a 🚀

---
+++ assets/markdown_english-word.md.08881402.js
@@ -0,0 +1,33 @@
+import{_ as s,j as n,g as a,G as l}from"./chunks/framework.791f7e63.js";const u='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"markdown/english-word.md","lastUpdated":1654442365000}',e={name:"markdown/english-word.md"},p=l(`<div class="language- line-numbers-mode"><span class="copy"></span><pre><code><span class="line"><span style="color:#A6ACCD;">20210910 \uFF08\u6211\u4E0D\u60F3\u6D6A\u8D39\u7684\u751F\u547D\uFF09</span></span>
+<span class="line"><span style="color:#A6ACCD;">I could stand here stark naked and I could yell as loud as I want and nobody cares</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u6211\u5C31\u7B97\u5149\u7740\u5C41\u80A1\uFF0C\u628A\u55D3\u5B50\u558A\u7834\uFF0C \u4E5F\u6CA1\u4EBA\u6CE8\u610F\u5230\u5427</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span>
+<span class="line"><span style="color:#A6ACCD;">when you leave here today call you parents</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u4ECA\u5929\u56DE\u5BB6\u540E\u544A\u8BC9\u4F60\u7239\u4F60\u5A18</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">and tell them you apologize for wasting their time</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u5411\u4ED6\u4EEC\u9053\u6B49 \u6D6A\u8D39\u4E86\u4ED6\u4EEC\u7684\u65F6\u95F4</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">and more importantly wasting their money sitting in this classroom learning absolutely nothing</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u6700\u7CDF\u7CD5\u7684\u662F\u5728\u8FD9\u82B1\u4ED6\u4EEC\u7684\u8840\u6C57\u94B1 \u72D7\u5C41\u90FD\u6CA1\u5B66\u5230</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">20210910</span></span>
+<span class="line"><span style="color:#A6ACCD;">Falling in love with you was the easiest thing I have ever done</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u7231\u4E0A\u4F60\u662F\u6211\u505A\u8FC7\u7684\u6700\u7B80\u5355\u7684\u4E8B</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">Nothing matters to me but you</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u6CA1\u6709\u4EC0\u4E48\u6BD4\u4F60\u66F4\u91CD\u8981</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">and every day I am alive, I am aware of this.</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u6211\u6D3B\u7740\u7684\u6BCF\u4E00\u5929\uFF0C\u90FD\u6DF1\u77E5\u8FD9\u4E00\u70B9</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">I love you the day I met you, I love you today.</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u7B2C\u4E00\u773C\u89C1\u5230\u4F60\u65F6\uFF0C\u6211\u5C31\u7231\u4E0A\u4F60\uFF0C\u4ECA\u5929\u4ECD\u7231\u4F60</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">and I will love you the rest of my life.</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u4ECA\u751F\u4ECA\u4E16\u90FD\u4F1A\u7231\u4F60</span></span>
+<span class="line"><span style="color:#A6ACCD;"> </span></span>
+<span class="line"><span style="color:#A6ACCD;">20210915</span></span>
+<span class="line"><span style="color:#A6ACCD;">are you single</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u4F60\u662F\u5355\u751F\u72D7\u5417 ?</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre><div class="line-numbers-wrapper"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br><span class="line-number">23</span><br><span class="line-number">24</span><br><span class="line-number">25</span><br><span class="line-number">26</span><br><span class="line-number">27</span><br><span class="line-number">28</span><br><span class="line-number">29</span><br><span class="line-number">30</span><br><span class="line-number">31</span><br><span class="line-number">32</span><br></div></div>`,1),r=[p];function o(c,i,t,b,A,C){return a(),n("div",null,r)}var y=s(e,[["render",o]]);export{u as __pageData,y as default};
